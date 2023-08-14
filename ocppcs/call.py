a640d8054c60ca1b64a37aaf263e52d4d4eef155
Oleg Tarasov <otarasov@sfxdx.com>
2023-08-15 00:35:53 +0300

    add new call methods

--- ocppcs/call.py
+++ ocppcs/call.py
@@ -10,6 +10,19 @@ def get_call_payload(inp):
     method=inp['method']
 
     match method:
+        case Action.ChangeAvailability:
+            return call.ChangeAvailabilityPayload(
+                type=params['type']
+            )
+        case Action.GetConfiguration:
+            return call.GetConfigurationPayload(
+                key=params['key']
+            )
+        case Action.ChangeConfiguration:
+            return call.ChangeConfigurationPayload(
+                key=[params['key']],
+                value=params['value'],
+            )
         case Action.RemoteStartTransaction:
             return call.RemoteStartTransactionPayload(
                 connector_id=params['connectorId'],
@@ -19,60 +32,48 @@ def get_call_payload(inp):
             return call.RemoteStopTransactionPayload(
                 transaction_id=params['transactionId'],
             )
-        case Action.GetConfiguration:
-            return call.GetConfigurationPayload(
-                key=params['key']
+        case Action.ClearCache:
+            return call.ClearCachePayload()
+        case Action.GetDiagnostics:
+            return call.GetDiagnosticsPayload(
+                location=params['location']
             )
-        case Action.ChangeConfiguration:
-            return call.ChangeConfigurationPayload(
-                key=params['key'],
-                value=params['value'],
+        case Action.Reset:
+            return call.ResetPayload(
+                type=params['type']
             )
-        case _:
-            print("not implemented!")
-            return None
-
-
-def get_call_result(response, method):
-
-    match method:
-        case Action.RemoteStartTransaction:
-            return {'status': response.status}
-        case Action.RemoteStopTransaction:
-            return {'status': response.status}
-        case Action.GetConfiguration:
-            return {'configurationKey': response.configuration_key}
-        case Action.ChangeConfiguration:
-            return {'status': response.status}
-        case _:
-            print("not implemented!")
-            return None
-
-
-def get_call_payload(inp):
-    # print("@@@@@@@@@@@@@@@@@@@@@@@@@@", 'params' in inp.keys())
-    if 'params' in inp.keys():
-        params = inp["params"]
-    method=inp['method']
-
-    match method:
-        case Action.RemoteStartTransaction:
-            return call.RemoteStartTransactionPayload(
+        case Action.UnlockConnector:
+            return call.UnlockConnectorPayload(
+                connector_id=params['connectorId']
+            )
+        case Action.ReserveNow:
+            return call.ReserveNowPayload(
                 connector_id=params['connectorId'],
-                id_tag=params['idTag']
+                expiry_date=params['expiryDate'],
+                id_tag=params['idTag'],
+                reservation_id=params['reservationId'],
             )
-        case Action.RemoteStopTransaction:
-            return call.RemoteStopTransactionPayload(
-                transaction_id=params['transactionId'],
+        case Action.CancelReservation:
+            return call.CancelReservationPayload(
+                reservation_id=params['reservationId'],
             )
-        case Action.GetConfiguration:
-            return call.GetConfigurationPayload(
-                key=params['key']
+        case Action.DataTransfer:
+            return call.DataTransferPayload(
+                vendor_id=params['vendorId'],
+                message_id=params['messageId'],
+                data=params['data'],
             )
-        case Action.ChangeConfiguration:
-            return call.ChangeConfigurationPayload(
-                key=params['key'],
-                value=params['value'],
+        case Action.GetLocalListVersion:
+            return call.GetLocalListVersionPayload()
+        case Action.SendLocalList:
+            return call.SendLocalListPayload(
+                list_version=params['listVersion'],
+                local_authorization_list=params['localAuthorizationList']
+            )
+        case Action.TriggerMessage:
+            return call.TriggerMessagePayload(
+                requested_message=params['requestedMessage'],
+                connector_id=params['connectorId']
             )
         case _:
             print("not implemented!")
@@ -82,6 +83,9 @@ def get_call_payload(inp):
 def get_call_result(response, method):
 
     match method:
+        case Action.ChangeAvailability:
+            call_result.ChangeAvailabilityPayload()
+            return {'status': response.status}
         case Action.RemoteStartTransaction:
             return {'status': response.status}
         case Action.RemoteStopTransaction:
@@ -90,6 +94,72 @@ def get_call_result(response, method):
             return {'configurationKey': response.configuration_key}
         case Action.ChangeConfiguration:
             return {'status': response.status}
+        case Action.ClearCache:
+            return {'status': response.status}
+        case Action.GetDiagnostics:
+            return {'fileName': response.file_name}
+        case Action.Reset:
+            return {'status': response.status}
+        case Action.UnlockConnector:
+            return {'status': response.status}
+        case Action.ReserveNow:
+            return {'status': response.status}
+        case Action.CancelReservation:
+            return {'status': response.status}
+        case Action.DataTransfer:
+            return {'status': response.status}
+        case Action.GetLocalListVersion:
+            return {'listVersion': response.list_version}
+        case Action.SendLocalList:
+            return {'status': response.status}
+        case Action.TriggerMessage:
+            return {'status': response.status}  
         case _:
             print("not implemented!")
             return None
+
+
+# def get_call_payload(inp):
+#     # print("@@@@@@@@@@@@@@@@@@@@@@@@@@", 'params' in inp.keys())
+#     if 'params' in inp.keys():
+#         params = inp["params"]
+#     method=inp['method']
+
+#     match method:
+#         case Action.RemoteStartTransaction:
+#             return call.RemoteStartTransactionPayload(
+#                 connector_id=params['connectorId'],
+#                 id_tag=params['idTag']
+#             )
+#         case Action.RemoteStopTransaction:
+#             return call.RemoteStopTransactionPayload(
+#                 transaction_id=params['transactionId'],
+#             )
+#         case Action.GetConfiguration:
+#             return call.GetConfigurationPayload(
+#                 key=params['key']
+#             )
+#         case Action.ChangeConfiguration:
+#             return call.ChangeConfigurationPayload(
+#                 key=params['key'],
+#                 value=params['value'],
+#             )
+#         case _:
+#             print("not implemented!")
+#             return None
+
+
+# def get_call_result(response, method):
+
+#     match method:
+#         case Action.RemoteStartTransaction:
+#             return {'status': response.status}
+#         case Action.RemoteStopTransaction:
+#             return {'status': response.status}
+#         case Action.GetConfiguration:
+#             return {'configurationKey': response.configuration_key}
+#         case Action.ChangeConfiguration:
+#             return {'status': response.status}
+#         case _:
+#             print("not implemented!")
+#             return None
