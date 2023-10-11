ce6e3715458d309d0c8b4bee4ab9168ba1a86b76
Oleg Tarasov <oo-tarasov@ya.ru>
2023-10-11 18:01:13 +0300

    test sse

---
+++ sse.py
@@ -0,0 +1,42 @@
+import asyncio
+from aiohttp import web
+from aiohttp.web import Response
+from aiohttp_sse import sse_response
+from datetime import datetime
+
+
+async def hello(request):
+    async with sse_response(request) as resp:
+        while True:
+            data = 'Server Time : {}'.format(datetime.now())
+            print(data)
+            await resp.send(data)
+            await asyncio.sleep(0)
+
+async def handle(request):
+    print ("handle trigger")
+    return Response(text='handle trigger', content_type='text/html')
+
+async def index(request):
+    d = """
+        <html>
+        <body>
+            <script>
+                var evtSource = new EventSource("/hello");
+                evtSource.onmessage = function(e) {
+                    document.getElementById('response').innerText = e.data
+                }
+            </script>
+            <h1>Response from server:</h1>
+            <div id="response"></div>
+        </body>
+    </html>
+    """
+    return Response(text=d, content_type='text/html')
+
+
+app = web.Application()
+app.router.add_route('GET', '/hello', hello)
+app.router.add_route('GET', '/handle', handle)
+app.router.add_route('GET', '/', index)
+web.run_app(app, host='127.0.0.1', port=8080)
